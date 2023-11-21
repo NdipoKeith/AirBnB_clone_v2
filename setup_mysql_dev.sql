c8c60f79201238d907faf611aaf06b3e6385cfec
DAVENJAGI <njagidave39@gmail.com>
2023-11-21 09:13:35 -0500

    AirBnB_v2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,8 @@
 --a script to prepare a MySQL server for the project.
+--Greant user privilege for perfomance_schema
+--not fail if database exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
