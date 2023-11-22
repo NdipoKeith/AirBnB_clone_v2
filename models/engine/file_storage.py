08d5ab820d1712793ee1914246093e0321177862
DAVENJAGI <njagidave39@gmail.com>
2023-11-22 17:13:04 -0500

    AirBnB_v2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -43,10 +43,8 @@ class FileStorage:
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
-            """
             for key, val in temp.items():
                 temp[key] = val.to_dict()
-                """
             json.dump(temp, f)
 
     def reload(self):
