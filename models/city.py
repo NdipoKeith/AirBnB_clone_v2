127ebe8b07f9667ff37d5cba308ccf3e0897f95c
DAVENJAGI <njagidave39@gmail.com>
2023-11-22 06:18:05 -0500

    AirBnB_v2

--- models/city.py
+++ models/city.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = "cities"
 
-    state_id = ""
-    name = ""
+    state_id = Column(String(128), nullable=False, ForeignKey(state.id))
+    name = Column(String(128), nullable=False)
