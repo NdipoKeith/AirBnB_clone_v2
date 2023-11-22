111b29cef5022284a683ac7ec821b50a671f6a22
DAVENJAGI <njagidave39@gmail.com>
2023-11-21 20:53:29 -0500

    AirBnB_v2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,14 +6,16 @@ import json
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
-    __objects = {}
+
+    def __init__(self):
+        self.__objects = {}
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is None:
-            return __objects
+            return self.__objects
         else:
-            return [obj for obj in __objects if isinstance(obj, cls)]
+            return [obj for obj in self.__objects if isinstance(obj, cls)]
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -43,16 +45,23 @@ class FileStorage:
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
                   }
+        self.__objects.clear()
+
         try:
-            temp = {}
+            temp = self.__objects.copy()
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
+                """
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                        self.__objects()[key] = classes[val['__class__']](**val)
+                        """
+                self.__objects = {key: classes[val['__class__']] for key, val in temp.items()}
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
         """method to delete object from _objects"""
         if obj is not None and obj in __objects:
-            __objects.remove(obj)
+            temp_objects = self.__objects.copy()
+            temp_objects.remove(obj)
+            self.__objects = temp_objects
