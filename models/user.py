db033366560884a61383f8e1130e27425c7a6257
DAVENJAGI <njagidave39@gmail.com>
2023-11-21 19:46:45 -0500

    AirBnB_v2

--- models/user.py
+++ models/user.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base()
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, INteger
 
-
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
