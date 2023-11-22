7fc51d60782e139aa2a988523476f83f3a744603
DAVENJAGI <njagidave39@gmail.com>
2023-11-22 07:49:19 -0500

    AirBnB_v2

--- models/city.py
+++ models/city.py
@@ -8,8 +8,10 @@ from sqlalchemy import Column, String
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-
-    state_id = Column(String(128), nullable=False, ForeignKey(state.id))
-    name = Column(String(128), nullable=False)
-
-    places = relationship("Place", casade="all, delete", backref="cities")
+    if type_storage == 'db':
+        state_id = Column(String(128), nullable=False, ForeignKey(state.id))
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", casade="all, delete", backref="cities")
+    else:
+        name = ""
+        state_id = ""
